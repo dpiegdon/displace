d40a18ec682195c23fd829ea5744afe40d9db68f
David R. Piegdon <dgit@piegdon.de>
2021-05-01 00:02:40 +0000

    configuration docs, usability

--- cfghelper.py
+++ cfghelper.py
@@ -15,12 +15,15 @@ class DesktopSetup:
         return self.__postexec
 
     def __str__(self):
-        return ("<DesktopSetup outputs:\n\t"
-                + "\n\t".join(("{} -> {}".format(p, c)
-                               for p, c in self.outputs.items()))
-                + "\n  postexec:\n\t"
-                + "\n\t".join(self.postexec)
-                + ">")
+        r = ("<DesktopSetup outputs:\n\t"
+             + "\n\t".join(("{} -> {}".format(p, c)
+                            for p, c in self.outputs.items())))
+        if self.postexec:
+             r += ("\n     postexec:\n\t"
+                   + "\n\t".join(self.postexec))
+
+        r += "\n     >"
+        return r
 
 
 def OutputSetup(name, **kwargs):
@@ -42,15 +45,3 @@ def OutputSetup(name, **kwargs):
     # location=(x,y) or (direction,port,align)
     # FIXME: make explicit params with sane defaults?
     return (name, kwargs)
-
-
-def setdpi(scale):
-    return [
-        (r"sed -i -e 's/^Xft.dpi:[ \t].*$/Xft.dpi: {}/'".format(
-            215 if scale[0] >= 1.5 else 120)
-         + r"               ~/.Xdefaults"),
-        (r"sed -i -e 's/^XTerm.faceSize:[ \t].*$/XTerm*faceSize: {}/'".format(
-            15 if scale[0] >= 1.5 else 10)
-         + r" ~/.Xdefaults"),
-        (r"xrdb ~/.Xdefaults")
-        ]
--- config.py
+++ config.py
@@ -2,63 +2,88 @@ from collections import OrderedDict
 
 from cfghelper import DesktopSetup as DESK
 from cfghelper import OutputSetup as OUT
-from cfghelper import setdpi
 
 
-def ATNA33TP06(primary=True, mode="max_area", **kwargs):
+# scale 1:1 if using a low-dpi display in low-dpi context
+lowdpi_scale = (1, 1)
+# downscale 2:1 from high-res if using a low-dpi display in high-dpi context
+highdpi_scale = (2, 2)
+
+
+def setdpi(scale):
+    return [
+        (r"sed -i -e 's/^Xft.dpi:[ \t].*$/Xft.dpi: {}/'".format(
+            215 if scale[0] >= 1.5 else 120)
+         + r"               ~/.Xdefaults"),
+        (r"sed -i -e 's/^XTerm.faceSize:[ \t].*$/XTerm*faceSize: {}/'".format(
+            15 if scale[0] >= 1.5 else 10)
+         + r"  ~/.Xdefaults"),
+        (r"xrdb ~/.Xdefaults")
+        ]
+
+
+# == laptop configurations ====================================================
+
+lowdpi_mode = (1920, 1080)
+highdpi_mode = "max_area"
+
+
+def ATNA33TP06(primary=True, mode=highdpi_mode, **kwargs):
+    """laptop main display"""
     return OUT("Text:ATNA33TP06-0", primary=primary, mode=mode, **kwargs)
 
 
-lowdpi_scale = (1, 1)
-highdpi_scale = (2, 2)
-
-highdpi_landscape = DESK(
-        ATNA33TP06(),
-        postexec=setdpi(highdpi_scale)
-        )
-
-lowdpi_landscape = DESK(
-        ATNA33TP06(mode=(2560, 1440)),
-        postexec=setdpi(lowdpi_scale)
-        )
-
-portrait = DESK(ATNA33TP06(rotate=True))
-
-highdpi_dock = DESK(
+dock_highdpi = DESK(
         OUT("Name:BenQ_LCD", scale=highdpi_scale, rotate="left"),
-        OUT("Name:DELL_U2515H", location=("right-of", "previous"),
-            scale=highdpi_scale),
+        OUT("Name:DELL_U2515H", scale=highdpi_scale,
+            location=("right-of", "previous")),
         ATNA33TP06(location=("below", "previous")),
         postexec=setdpi(highdpi_scale)
         )
 
-
-lowdpi_dock = DESK(
+dock_lowdpi = DESK(
         OUT("Name:BenQ_LCD", scale=lowdpi_scale, rotate="left"),
         OUT("Name:DELL_U2515H", location=("right-of", "previous"),
             scale=lowdpi_scale),
-        ATNA33TP06(location=("below", "previous"), mode=(2560, 1440)),
+        ATNA33TP06(location=("below", "previous"), mode=lowdpi_mode),
         postexec=setdpi(lowdpi_scale)
         )
 
-present_right = DESK(
+landscape_highdpi = DESK(
+        ATNA33TP06(),
+        postexec=setdpi(highdpi_scale)
+        )
+
+landscape_lowdpi = DESK(
+        ATNA33TP06(mode=lowdpi_mode),
+        postexec=setdpi(lowdpi_scale)
+        )
+
+portrait_highdpi = DESK(
+        ATNA33TP06(rotate=True),
+        postexec=setdpi(lowdpi_scale)
+        )
+
+present_left_highdpi = DESK(
+        OUT("HDMI-1", scale=highdpi_scale),
+        ATNA33TP06(location=("right-of", "HDMI-1")),
+        postexec=setdpi(highdpi_scale)
+        )
+
+present_top_highdpi = DESK(
+        OUT("HDMI-1", scale=highdpi_scale),
+        ATNA33TP06(location=("below", "HDMI-1")),
+        postexec=setdpi(highdpi_scale)
+        )
+
+present_right_highdpi = DESK(
         ATNA33TP06(),
         OUT("HDMI-1", scale=highdpi_scale,
             location=("right-of", "Text:ATNA33TP06-0")),
         postexec=setdpi(highdpi_scale)
         )
 
-present_top = DESK(
-        OUT("HDMI-1", scale=highdpi_scale),
-        ATNA33TP06(location=("below", "HDMI-1")),
-        postexec=setdpi(highdpi_scale)
-        )
-
-present_left = DESK(
-        OUT("HDMI-1", scale=highdpi_scale),
-        ATNA33TP06(location=("right-of", "HDMI-1")),
-        postexec=setdpi(highdpi_scale)
-        )
+# == home desktop configurations ==============================================
 
 central = DESK(
         OUT("Name:BenQ_LCD", rotate="left", primary=True),
@@ -67,18 +92,26 @@ central = DESK(
         postexec=[]
         )
 
+# =============================================================================
 
 # order defines priority for --auto:
 defined_setups = OrderedDict([
-        ("hidock",            highdpi_dock),
-        ("lodock",            lowdpi_dock),
-        ("hiscape",           highdpi_landscape),
-        ("loscape",           lowdpi_landscape),
-        ("portrait",          portrait),
-        ("present_right",     present_right),
-        ("present_top",       present_top),
-        ("central",           central)])
+        # laptop
+        ("dock",              dock_highdpi),
+        ("dock-lowdpi",       dock_lowdpi),
+        ("landscape",         landscape_highdpi),
+        ("landscape-lowdpi",  landscape_lowdpi),
+        ("portrait",          portrait_highdpi),
+        ("present-left",      present_left_highdpi),
+        ("present-top",       present_top_highdpi),
+        ("present-right",     present_right_highdpi),
+        # home desktop
+        ("central",           central),
+        ])
 
-
-postexec_all = ["ps aux|grep qtile|grep -v grep > /dev/null"
-                + " && ~/.qtile/qtile/bin/qshell -c 'restart()'"]
+postexec_all = [
+                # Restart qtile so it reconfigures for new layout.
+                # Might be better to use a qtile hook in its configuration.
+                ("ps aux|grep qtile|grep -v grep > /dev/null"
+                 + " && ~/.qtile/qtile/bin/qshell -c 'restart()'")
+                ]
